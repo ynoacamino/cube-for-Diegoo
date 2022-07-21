2342598a17f7225ddfe625690a7dea4b731421fb
Diegoo11 <ynoacamino@gmail.com>
2022-07-21 07:46:17 -0500

    estilized

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Clock from './Clock'
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Clock from './Clock';
 
 function App() {
-  const [count, setCount] = useState(false)
-  const [stop, setStop] = useState(0)
+  const [count, setCount] = useState(false);
+  const [stop, setStop] = useState(0);
 
-  const [live, setLive] = useState(false)
-  const [save, setSave] = useState(false)
+  const [live, setLive] = useState(false);
+  //  const [save, setSave] = useState(false);
 
   const start = () => {
-    if(!live) {
-      setCount(true)
-      setLive(true)
-    } else if(live) {
-      setCount(false)
-      setLive(false)
+    if (!live) {
+      setCount(true);
+      setLive(true);
+    } else if (live) {
+      setCount(false);
+      setLive(false);
     }
-    
-  }
+  };
 
   const handle = (aa) => {
-    setStop(aa)
-  }
+    setStop(aa);
+  };
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => start()}>
+        <button onClick={() => start()} type="button">
           count
         </button>
 
-        <p> 
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p>
+          Edit
+          {' '}
+          <code>src/App.jsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       {!count
-      &&
-      <h1>{Math.floor(stop / 100) > 9 ? Math.floor(stop / 100) : "0" + Math.floor(stop / 100) } . {stop%100 > 9 ? stop%100 : "0" + stop%100}</h1>}
+      && (
+      <h1>
+        {Math.floor(stop / 100) > 9 ? Math.floor(stop / 100) : `0${Math.floor(stop / 100)}` }
+        {' '}
+        .
+        {' '}
+        {stop % 100 > 9 ? stop % 100 : `0${stop % 100}`}
+      </h1>
+      )}
       {count
-      &&
-      <Clock men={(ee) => handle(ee)}/>}
+      && <Clock men={(ee) => handle(ee)} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
