1be1241fe0aa8c1105a0d4c8edb32d95638208e4
Diegoo11 <ynoacamino@gmail.com>
2022-08-01 13:50:00 -0500

    scramble logical in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header/Header';
 import Gif from './Gif/Gif';
 import ModuleTime from './ModuleTime/ModuleTime';
+import desarmador from './Scramble/main';
 
 import Scramble from './Scramble/Scramble';
 
 function App() {
+  const [scram, setScram] = useState(desarmador());
+  const end = (time) => {
+    console.log(time, scram);
+    setScram(desarmador());
+  };
   return (
     <>
       <Header />
       <div className="App">
         <Gif />
-        <Scramble />
-        <ModuleTime />
+        <Scramble scramble={scram} />
+        <ModuleTime end={(time) => end(time)} />
         <span className="instruccionesApp">Presiona spacio</span>
       </div>
     </>
