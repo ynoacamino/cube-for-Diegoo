17fcae977a0daf9730216aad6491a0f5c50b665f
Diegoo11 <ynoacamino@gmail.com>
2022-08-02 14:05:09 -0500

    new modal box

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,17 @@
 import './Header.css';
-import React from 'react';
+import React, { useState } from 'react';
 import LogoCube from '../imgs/logoCube2.svg';
 import Settings from '../imgs/settings.png';
+import LateralBar from './LateralBar/LateralBar';
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
+  const clickOpen = () => {
+    setOpen(!open);
+    console.log(open);
+  };
+
   return (
     <div className="Header">
       <div className="rightHeader">
@@ -14,6 +22,7 @@ function Header() {
         <button
           className="btnSettingsHeader pointer"
           type="button"
+          onClick={clickOpen}
         >
           <img
             src={Settings}
@@ -28,6 +37,8 @@ function Header() {
           Log in
         </button>
       </div>
+      <div aria-hidden="true" onClick={clickOpen} className={`boxNormalModal ${open && 'boxShadowModal'}`} />
+      <LateralBar open={open} func={clickOpen} />
     </div>
   );
 }
