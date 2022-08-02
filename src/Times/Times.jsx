34ae7a1c11a32abc4d063d8cef3310c753238be5
Diegoo11 <ynoacamino@gmail.com>
2022-08-02 14:06:31 -0500

    new component

---
+++ src/Times/Times.jsx
@@ -0,0 +1,28 @@
+import './Times.css';
+import React from 'react';
+import trashBin from '../imgs/trash-bin.png';
+import SingleTime from './SingleTime/SingleTime';
+
+function Times({ times }) {
+  return (
+    <div className="Times">
+      <div className="headerTimes">
+        <span className="titleTimes">Times</span>
+        <button type="button" className="btnRemoveTimes pointer">
+          <img src={trashBin} alt="icoTrash" className="icoBtnRemove" />
+        </button>
+      </div>
+      {times.length !== 0
+      && times.map((single, index) => (
+        <SingleTime
+          num={times.length - (index)}
+          time={single.time}
+          scramble={single.scram}
+        />
+      ))}
+      {times.length === 0 && <span>Aun no hay datos</span>}
+    </div>
+  );
+}
+
+export default Times;
