5ef3f7db55483ffa6e2b83eecc677f52c0b86fd0
Diegoo11 <ynoacamino@gmail.com>
2022-08-03 07:22:27 -0500

    logic for boton eeset

--- src/Times/Times.jsx
+++ src/Times/Times.jsx
@@ -3,18 +3,23 @@ import React from 'react';
 import trashBin from '../imgs/trash-bin.png';
 import SingleTime from './SingleTime/SingleTime';
 
-function Times({ times }) {
+function Times({ times, reset }) {
   return (
     <div className="Times">
       <div className="headerTimes">
         <span className="titleTimes">Times</span>
-        <button type="button" className="btnRemoveTimes pointer">
+        <button
+          type="button"
+          className="btnRemoveTimes pointer"
+          onClick={reset}
+        >
           <img src={trashBin} alt="icoTrash" className="icoBtnRemove" />
         </button>
       </div>
       {times.length !== 0
       && times.map((single, index) => (
         <SingleTime
+          key={single.scram}
           num={times.length - (index)}
           time={single.time}
           scramble={single.scram}
