147c689a9f59ea9f3f9c9e38825748552e986793
Diegoo11 <ynoacamino@gmail.com>
2022-08-04 13:15:53 -0500

    microseg => segs

--- src/Times/SingleTime/SingleTime.jsx
+++ src/Times/SingleTime/SingleTime.jsx
@@ -1,15 +1,39 @@
 import './SingleTime.css';
 import React from 'react';
+import trash from '../../imgs/trash-bin.png';
+import dnf from '../../imgs/DNF.svg';
+import two from '../../imgs/moreTwo.svg';
 
-function SingleTime({ num, time, scramble }) {
+function SingleTime({
+  num, time, scramble, removeSingle, moreTwo, DNF,
+}) {
   return (
     <div className="SingleTime">
-      <span className="numSingleTime">
-        {num}
-        :
-      </span>
-      <span className="timeSingleTime">{time}</span>
-      <span className="scrambleSingleTime">{scramble}</span>
+      <div className="rigthTime">
+        <span className="numSingleTime">
+          {num}
+          :
+        </span>
+        <span className="timeSingleTime">
+          <span className="partTime">{Math.floor(time / 100) > 9 ? Math.floor(time / 100) : `0${Math.floor(time / 100)}` }</span>
+          .
+          <span className="partTime">
+            {time % 100 > 9 ? time % 100 : `0${time % 100}`}
+          </span>
+        </span>
+        <span className="scrambleSingleTime">{scramble}</span>
+      </div>
+      <div className="leftTime">
+        <button className="btnTime pointer btnMoreTwo" type="button" onClick={moreTwo}>
+          <img className="icoBtnTime" src={two} alt="moreTwo" />
+        </button>
+        <button className="btnTime pointer" type="button" onClick={DNF}>
+          <img className="icoBtnTime" src={dnf} alt="DNF" />
+        </button>
+        <button className="btnTime pointer" type="button" onClick={removeSingle}>
+          <img className="icoBtnTime" src={trash} alt="remove" />
+        </button>
+      </div>
     </div>
   );
 }
