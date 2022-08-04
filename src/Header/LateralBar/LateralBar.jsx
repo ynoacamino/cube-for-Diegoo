6c6709cc8472f3517f7c9f5a3f6c67a1235ecb21
Diegoo11 <ynoacamino@gmail.com>
2022-08-04 13:13:23 -0500

    new structure

--- src/Header/LateralBar/LateralBar.jsx
+++ src/Header/LateralBar/LateralBar.jsx
@@ -1,25 +1,44 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './LateralBar.css';
 import closeIco from '../../imgs/close.png';
 
 function LateralBar({ open, func }) {
   return (
-    <ul className={`NavMobile ${open && 'NavMobileView'}`}>
-      <li className="li">
-        <a className=" navLink " href="/">Inicio</a>
-      </li>
-      <li className="li">
-        <a className=" navLink " href="/">Productos</a>
-      </li>
-      <li className="li">
-        <a className=" navLink " href="/">Marcas</a>
-      </li>
-      <li className="li">
-        <a className=" navLink " href="/">Tiendas</a>
-      </li>
-      <li className="li">
-        <a className=" navLink " href="/">Comprar</a>
-      </li>
+    <div className={`NavMobile ${open && 'NavMobileView'}`}>
+      <div className="boxSettings">
+        <span>Theme:</span>
+        <div className="optionsTheme">
+          <label htmlFor="white">
+            <input type="radio" name="theme" id="white" />
+            Light theme
+          </label>
+
+          <label htmlFor="dark">
+            <input type="radio" name="theme" id="dark" />
+            Dark theme
+          </label>
+        </div>
+        <hr />
+        <span>Cube:</span>
+        <div className="optionsCube">
+
+          <label htmlFor="2x2x2">
+            <input type="radio" name="theme" id="2x2x2" />
+            2x2x2 cube
+          </label>
+
+          <label htmlFor="3x3x3">
+            <input type="radio" name="theme" id="3x3x3" />
+            3x3x3 cube
+          </label>
+
+          <label htmlFor="4x4x4">
+            <input type="radio" name="theme" id="4x4x4" />
+            4x4x4 cube
+          </label>
+        </div>
+      </div>
       <button
         type="button"
         className="btnExit pointer"
@@ -27,7 +46,7 @@ function LateralBar({ open, func }) {
       >
         <img src={closeIco} alt="close" className="imgCloseIco" />
       </button>
-    </ul>
+    </div>
   );
 }
 
