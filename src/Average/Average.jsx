8e3b9a1fd6815e8244afcbb984ac5348e659aeda
Diegoo11 <ynoacamino@gmail.com>
2022-08-08 09:30:22 -0500

    moved

---
+++ src/Average/Average.jsx
@@ -0,0 +1,33 @@
+import './Average.css';
+import React from 'react';
+
+function Average({
+  statistics,
+}) {
+  return (
+    <div className="Average">
+      <div className="boxAverage">
+        <span className="spanNameAverage">Media:</span>
+        <span className="spanNumAverage">{statistics.media || '- -'}</span>
+      </div>
+      <div className="boxAverage">
+        <span className="spanNameAverage">Mejor:</span>
+        <span className="spanNumAverage">{statistics.best === 999999 ? '- -' : statistics.best}</span>
+      </div>
+      <div className="boxAverage">
+        <span className="spanNameAverage">Ao5:</span>
+        <span className="spanNumAverage">{statistics.ao5 || '- -'}</span>
+      </div>
+      <div className="boxAverage">
+        <span className="spanNameAverage">Ao12:</span>
+        <span className="spanNumAverage">{statistics.ao12 || '- -'}</span>
+      </div>
+      <div className="boxAverage">
+        <span className="spanNameAverage">Cuenta:</span>
+        <span className="spanNumAverage">{statistics.count || '- -'}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Average;
