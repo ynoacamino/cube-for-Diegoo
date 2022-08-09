2d114d7a9551b5aea54fe1784b706295c0d4f31f
Diegoo11 <ynoacamino@gmail.com>
2022-08-09 14:07:04 -0500

    point decimal

--- src/Average/Average.jsx
+++ src/Average/Average.jsx
@@ -8,19 +8,19 @@ function Average({
     <div className="Average">
       <div className="boxAverage">
         <span className="spanNameAverage">Media:</span>
-        <span className="spanNumAverage">{statistics.media || '- -'}</span>
+        <span className="spanNumAverage">{statistics.media / 100 || '- -'}</span>
       </div>
       <div className="boxAverage">
         <span className="spanNameAverage">Mejor:</span>
-        <span className="spanNumAverage">{statistics.best === 999999 ? '- -' : statistics.best}</span>
+        <span className="spanNumAverage">{statistics.best === 999999 ? '- -' : statistics.best / 100}</span>
       </div>
       <div className="boxAverage">
         <span className="spanNameAverage">Ao5:</span>
-        <span className="spanNumAverage">{statistics.ao5 || '- -'}</span>
+        <span className="spanNumAverage">{statistics.ao5 / 100 || '- -'}</span>
       </div>
       <div className="boxAverage">
         <span className="spanNameAverage">Ao12:</span>
-        <span className="spanNumAverage">{statistics.ao12 || '- -'}</span>
+        <span className="spanNumAverage">{statistics.ao12 / 100 || '- -'}</span>
       </div>
       <div className="boxAverage">
         <span className="spanNameAverage">Cuenta:</span>
