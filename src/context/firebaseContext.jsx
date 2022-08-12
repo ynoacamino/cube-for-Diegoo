79c293fd1b1cfec43ae0c1e069826b106442f961
Diegoo11 <ynoacamino@gmail.com>
2022-08-12 09:10:54 -0500

    create context for firebse in react

---
+++ src/context/firebaseContext.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import {
+  React, createContext, useContext, useEffect, useState,
+} from 'react';
+import { auth } from '../firebaseConfig';
+
+const authContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(authContext);
+  if (!context) throw new Error('There is no Auth provider');
+  return context;
+};
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const loginWithGoogle = async () => {
+    const googleProvider = new GoogleAuthProvider();
+    await signInWithPopup(auth, googleProvider);
+  };
+
+  const logout = () => signOut(auth);
+
+  useEffect(() => {
+    const unsubuscribre = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      console.log(currentUser);
+    });
+    return () => unsubuscribre();
+  }, []);
+  return (
+    <authContext.Provider
+      value={{
+        user,
+        loading,
+        loginWithGoogle,
+        logout,
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+}
