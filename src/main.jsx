0c6708ed5afa9d51df9abdffb858bd970fa857a1
Diegoo11 <ynoacamino@gmail.com>
2022-08-13 13:50:22 -0500

    authprovider added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { AuthProvider } from './context/firebaseContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <App />,
-
+  <AuthProvider>
+    <App />
+    ,
+  </AuthProvider>,
 );
